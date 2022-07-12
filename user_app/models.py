59bc8b706528d15269be52d2c234363d22613b6d
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2022-07-12 18:18:29 +0300

    Add Signals to Profiles

--- user_app/models.py
+++ user_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # create Extends User Model
@@ -10,8 +12,16 @@ class Profile(models.Model):
     gender_selecation = (('Female', 'Female'),
                          ('Male', 'Male'))
     gender = models.CharField(
-        choices=gender_selecation, max_length=20, default='Male')
-    dob = models.DateField()
+        choices=gender_selecation, max_length=20, null=True, blank=True)
+    dob = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
